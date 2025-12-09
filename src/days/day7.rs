1eef7686a82eeb073856137db2954139e97d8dfc
Chris <86408925+ChrisBosman@users.noreply.github.com>
2025-12-09 11:31:35 +0100

    Finished day 8

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -1,4 +1,127 @@
 pub(crate) 
 fn run(input: &String) -> (usize, usize){ 
-    return (0, 0); 
-} 
+    let matrix: Vec<Vec<u8>> = input.lines().map(|line| line.chars().map(|c| if c == '^' {1} else if c=='S' {2} else {0}).collect()).collect();
+    let val = matrix.iter().flatten().position(|v| *v==2).expect("[Error] No Starting position found");
+    let starting_index = (val / matrix[0].len(), val % matrix[0].len());
+
+    let sol1 = part1(&matrix, starting_index);
+    let sol2 = part2(&matrix, starting_index);
+
+    // for i in 0..matrix.len() {
+    //     for j in 0..matrix[i].len() {
+    //         print!(" {}",match mod_matrix[i][j] {
+    //             1 => {'^'},
+    //             2 => {'S'},
+    //             3 => {'v'},
+    //             _ => {'.'},
+    //         })
+    //     }
+    //     print!("\n");
+    // }
+
+    return (sol1, sol2); 
+    // 3178 too low
+}
+
+fn part2(matrix: &Vec<Vec<u8>>, starting_index: (usize, usize)) -> usize {
+    // Part 2, Just depth first and increment count when reaching the bottom
+    let mut mod_matrix: Vec<Vec<usize>> = vec![vec![0; matrix[0].len()];matrix.len()];
+    depth_first(starting_index.0, starting_index.1, matrix, &mut mod_matrix)
+}
+
+fn breath_first(starting_index: (usize, usize), matrix: &Vec<Vec<u8>>) -> usize{
+    let mut sum = 0;
+    // let frontier = 
+
+
+    return sum;
+}
+
+fn depth_first(i: usize, j: usize, matrix: &Vec<Vec<u8>>, mod_matrix: &mut Vec<Vec<usize>>) -> usize{
+    let mut sum = 0;
+    // println!("Hi");
+    for y in (i+1)..matrix.len(){
+        if y == matrix.len()-1 {return 1;}
+        if matrix[y][j] == 0 {continue;} 
+        if mod_matrix[y][j] != 0 {
+            sum += mod_matrix[y][j];
+            break;
+        }
+        
+        // Check left
+        let mut left_val = 0;
+        if j>0 {
+            left_val = depth_first(y, j-1, matrix, mod_matrix);
+        }
+        // Check right
+        let mut right_val = 0;
+        if j<matrix[y].len()-1{
+            right_val = depth_first(y, j+1, matrix, mod_matrix);
+        }
+
+        // Update mod_matrix
+        mod_matrix[i][j] = left_val+right_val;
+
+        // Update sum
+        sum += mod_matrix[i][j];
+        break;
+    }
+    sum
+}
+
+fn part1(matrix: &Vec<Vec<u8>>, starting_index: (usize, usize)) -> usize {
+    // Part 1, Plan, start at the bottom, and move up, depth first, marking the point that are valid
+    let mut mod_matrix = matrix.clone();
+    
+    for i in (0..matrix.len()).rev() {
+        for j in 0..matrix[i].len(){
+            if mod_matrix[i][j] == 1 {
+                check_depth_first(i, j, &mut mod_matrix, starting_index);
+            }
+        }
+    }
+    
+    let sol1 = mod_matrix.iter().flatten().map(|val| if *val == 3 {1} else {0} ).sum();
+    sol1
+}
+
+// Checks if the splitter gets hit
+fn check_depth_first(i: usize,j: usize,mod_matrix: &mut Vec<Vec<u8>>,starting_index: (usize,usize)) -> bool{
+    // Move up
+    for y in (0..i).rev(){
+        // If we pass the start line/point
+        if y == starting_index.0{
+            if j == starting_index.1 {
+                mod_matrix[i][j] = 3;
+                return true;
+            }
+            break;
+        }
+        // If we encounter a splitter
+        if mod_matrix[y][j] != 0 {break;}
+
+        if j<mod_matrix[y].len()-1 && mod_matrix[y][j+1] > 0 {
+            // Point of interest
+            if mod_matrix[y][j+1] > 1 || check_depth_first(y, j+1, mod_matrix, starting_index){
+                mod_matrix[i][j] = 3;
+                return true;
+            }
+        }
+        if j>0 && mod_matrix[y][j-1] > 0 {
+            // Point of interest
+            if mod_matrix[y][j-1] > 1 || check_depth_first(y, j-1, mod_matrix, starting_index){
+                mod_matrix[i][j] = 3;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+
+// 1: 2
+// 2: 3
+// 3: 4 
+// 4: 5 or 6
+// 6: 8 (two beams hit the same thing)
--- src/days/day8.rs
+++ src/days/day8.rs
@@ -1,4 +1,84 @@
+#[derive(Default, Clone, Debug)]
+struct Pair{
+    p1:usize,
+    p2:usize,
+    dist: usize
+}
+
 pub(crate) 
 fn run(input: &String) -> (usize, usize){ 
-    return (0, 0); 
+    let nr_connections_to_make = if input.len() > 250 {1000} else {10};
+    let points: Vec<Vec<usize>> = input.lines().map(|line| line.split(',').map(|s| s.parse().unwrap()).collect()).collect();
+    let mut distances =  Vec::new();
+    for i in 0..points.len()-1{
+        for j in i+1..points.len(){ 
+            distances.push(Pair {p1: i, p2: j, dist: calculate_distance(&points[i],&points[j])});
+        }
+    };
+
+    distances.sort_unstable_by_key(|v| v.dist);
+    let mut networks = connect_points(&points, &distances, nr_connections_to_make).ok().unwrap();
+    networks.sort_unstable_by_key(|a| a.len());
+
+    let sol1 = networks.iter().skip(networks.len()-3).map(|arr| arr.len()).product();
+
+    let sol2 = match connect_points(&points, &distances, 1000000) {
+        Ok(_) => {println!("Not enough connections made"); 0},
+        Err(val) => val,
+    };
+    return (sol1, sol2); 
 } 
+
+fn connect_points(points: &Vec<Vec<usize>>, distances: &Vec<Pair>, nr_connections_to_make: usize) -> Result<Vec<Vec<usize>>, usize>{
+    let mut unconnected_points: Vec<usize> = (0..points.len()).collect();
+    let mut networks: Vec<Vec<usize>> = Vec::new();
+    for i in 0..nr_connections_to_make{
+        let pair = &distances[i];
+        // See if none are already in a network
+        if unconnected_points.contains(&pair.p1) && unconnected_points.contains(&pair.p2){
+            networks.push(vec![pair.p1,pair.p2]);
+            unconnected_points.swap_remove(unconnected_points.iter().position(|v| *v == pair.p1).unwrap());
+            unconnected_points.swap_remove(unconnected_points.iter().position(|v| *v == pair.p2).unwrap());
+        } else 
+        // If one is in a network
+        if unconnected_points.contains(&pair.p1){
+            for network in &mut networks{
+                if !network.contains(&pair.p2) {continue;}
+                network.push(pair.p1);
+            }
+            unconnected_points.swap_remove(unconnected_points.iter().position(|v| *v == pair.p1).unwrap());
+        } else
+        if unconnected_points.contains(&pair.p2){
+            for network in &mut networks{
+                if !network.contains(&pair.p1) {continue;}
+                network.push(pair.p2);
+            }
+            unconnected_points.swap_remove(unconnected_points.iter().position(|v| *v == pair.p2).unwrap());
+        } else {
+            // If both are in a network
+            let mut n1 = find_parent_network(&networks, &pair.p1);
+            let mut n2 = find_parent_network(&networks, &pair.p2);
+            if n1 == n2 {continue;}
+            if n2 < n1 {let tmp = n1; n1 = n2; n2=tmp;}
+            let mut network = networks.swap_remove(n2);
+            networks[n1].append(&mut network);
+        }
+        if unconnected_points.is_empty() && networks.len() == 1 {
+            return Err(points[pair.p1][0]*points[pair.p2][0]);
+        }
+    }
+    return Ok(networks);
+}
+
+fn find_parent_network(networks: &Vec<Vec<usize>>, point: &usize) -> usize {
+    for net_i in 0..networks.len(){
+        if networks[net_i].contains(point) {return net_i;}
+    }
+    return usize::MAX;
+}
+
+fn calculate_distance(p1: &Vec<usize>, p2: &Vec<usize>) -> usize{
+    p1.iter().zip(p2.iter()).map(|(v1,v2)|{
+        if v2 > v1 {(v2-v1)*(v2-v1)} else {(v1-v2)*(v1-v2)}
+    }).sum()
+}
